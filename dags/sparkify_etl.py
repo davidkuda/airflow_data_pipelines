614373fdd1360388b66eaff2214329f0d41ab44b
David Kuda <david.kuda@pm.me>
2021-11-06 17:54:52 +0100

    refactor: Use context manager

--- dags/sparkify_etl.py
+++ dags/sparkify_etl.py
@@ -24,53 +24,46 @@ default_args = {
     'email_on_retry': False,
 }
 
-dag = DAG('sparkify etl',
-          default_args=default_args,
-          description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *',
-          catchup=False
-        )
+with DAG(
+    'sparkify etl',
+    default_args=default_args,
+    description='Load and transform data in Redshift with Airflow',
+    schedule_interval='0 * * * *',
+    catchup=False
+) as dag:
 
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+    start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
-stage_events_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_events',
-    dag=dag
-)
+    stage_events_to_redshift = StageToRedshiftOperator(
+        task_id='Stage_events'
+    )
 
-stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_songs',
-    dag=dag
-)
+    stage_songs_to_redshift = StageToRedshiftOperator(
+        task_id='Stage_songs'
+    )
 
-load_songplays_table = LoadFactOperator(
-    task_id='Load_songplays_fact_table',
-    dag=dag
-)
+    load_songplays_table = LoadFactOperator(
+        task_id='Load_songplays_fact_table'
+    )
 
-load_user_dimension_table = LoadDimensionOperator(
-    task_id='Load_user_dim_table',
-    dag=dag
-)
+    load_user_dimension_table = LoadDimensionOperator(
+        task_id='Load_user_dim_table'
+    )
 
-load_song_dimension_table = LoadDimensionOperator(
-    task_id='Load_song_dim_table',
-    dag=dag
-)
+    load_song_dimension_table = LoadDimensionOperator(
+        task_id='Load_song_dim_table'
+    )
 
-load_artist_dimension_table = LoadDimensionOperator(
-    task_id='Load_artist_dim_table',
-    dag=dag
-)
+    load_artist_dimension_table = LoadDimensionOperator(
+        task_id='Load_artist_dim_table'
+    )
 
-load_time_dimension_table = LoadDimensionOperator(
-    task_id='Load_time_dim_table',
-    dag=dag
-)
+    load_time_dimension_table = LoadDimensionOperator(
+        task_id='Load_time_dim_table'
+    )
 
-run_quality_checks = DataQualityOperator(
-    task_id='Run_data_quality_checks',
-    dag=dag
-)
+    run_quality_checks = DataQualityOperator(
+        task_id='Run_data_quality_checks'
+    )
 
-end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+    end_operator = DummyOperator(task_id='Stop_execution')
