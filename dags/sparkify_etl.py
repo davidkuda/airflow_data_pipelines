11ed98e38bbbe2ad96fdd7d9d3bc435f41af5f13
David Kuda <david.kuda@pm.me>
2021-11-12 17:40:28 +0100

    Remove unused imports

--- dags/sparkify_etl.py
+++ dags/sparkify_etl.py
@@ -2,8 +2,6 @@ from datetime import datetime, timedelta
 import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-# from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                                # LoadDimensionOperator, DataQualityOperator)
 
 from operators.stage_redshift import StageToRedshiftOperator
 from operators.load_fact import LoadFactOperator
